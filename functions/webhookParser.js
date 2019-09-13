f66a628c47e4f2cecd4946c907f9d812cef02f5c
ramigs <ramigs@gmail.com>
2019-09-13 23:19:04 +0100

    Trying to console.log Webhook POST request from GitHub

--- functions/webhookParser.js
+++ functions/webhookParser.js
@@ -1,16 +1,17 @@
 exports.handler = function(event, context, callback) {
 
-    const { ref } = JSON.parse(event.body)
+/*     const { ref } = JSON.parse(event.body)
 
     const send = ref => {
         callback(null, {
             statusCode: 200,
             body: JSON.stringify({ msg: 'Hello ' + ref})
         });
-    }
+    } */
 
     // Make sure HTTP method is POST
     if (event.httpMethod == 'POST') {
-        send(ref);
+        console.log(event.body);
+        //send(ref);
     }
 }
