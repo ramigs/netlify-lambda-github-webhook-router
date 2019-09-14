579bd7e70bf87f267f5fdf895a9bbba98950288c
ramigs <ramigs@gmail.com>
2019-09-14 21:45:46 +0100

    List of added, removed, and modified files parsed

--- functions/webhookParser.js
+++ functions/webhookParser.js
@@ -1,17 +1,28 @@
 exports.handler = function(event, context, callback) {
 
-    const { head_commit } = JSON.parse(event.body)
+    const { head_commit: { added, removed, modified } } = JSON.parse(event.body)
 
-    /* const send = ref => {
+/*     const send = (a, r, m) => {
         callback(null, {
             statusCode: 200,
-            body: JSON.stringify({ msg: 'Hello ' + ref})
+            body: JSON.stringify({"added": a, "removed": r, "modified": m})
         });
     } */
 
+    const send = (a, r, m) => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    added: a,
+                    removed: r,
+                    modified: m
+                })
+        });
+    }
+
     // Make sure HTTP method is POST
     if (event.httpMethod == 'POST') {
-        console.log(head_commit);
-        //send(ref);
+        send(added, removed, modified);
     }
 }
