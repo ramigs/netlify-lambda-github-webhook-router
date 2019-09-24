429ab3ff9ae74a6594b5cb93a9b3b9bcf433189d
ramigs <ramigs@gmail.com>
2019-09-24 18:59:18 +0100

    dotenv installed, sites and endpoints are now being read from .env file

--- functions/webhookParser.js
+++ functions/webhookParser.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const fetch = require('node-fetch').default;
 
 exports.handler = function(event, context, callback) {
 
-    let sites = [
+    const { head_commit: { added, removed, modified } } = JSON.parse(event.body)
+
+    /* let sites = [
         {
             siteFolder: "01-JavaScript-Drum-Kit",
             netlifyEndPoint: "https://api.netlify.com/build_hooks/5d7e2fff8202d6a2a653d93f"
@@ -11,10 +14,22 @@ exports.handler = function(event, context, callback) {
             siteFolder: "02-JS-and-CSS-Clock",
             netlifyEndPoint: "https://api.netlify.com/build_hooks/5d7fb3beeccc183b2829a418"
         }
-    ]
+    ] */
+    
+    let siteFolder = process.env['SITE_1_FOLDER'];
+    let siteNetlifyEndpoint = process.env['SITE_1_NETLIFY_ENDPOINT'];
 
-    const { head_commit: { added, removed, modified } } = JSON.parse(event.body)
+    let sites = [];
 
+    for (let i = 2; siteFolder || siteNetlifyEndpoint; i++) {
+        sites.push({
+            siteFolder: siteFolder,
+            netlifyEndPoint: siteNetlifyEndpoint
+        });
+        siteFolder = process.env[`SITE_${i}_FOLDER`];
+        siteNetlifyEndpoint = process.env[`SITE_${i}_NETLIFY_ENDPOINT`];
+    }
+    
     const send = (a, r, m) => {
         callback(null, {
             statusCode: 200,
@@ -48,7 +63,7 @@ exports.handler = function(event, context, callback) {
                 console.log(netlifyEndPoint);
         
                 try {
-                    const result = await fetch(netlifyEndPoint, fetchOptions).then(console.log);
+                    await fetch(netlifyEndPoint, fetchOptions).then(console.log);
                 } catch (error) {
                   console.log({ error });
                 }
